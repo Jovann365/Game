0e518581742b70b5552ea313f04825dca5156853
Jovan Velichkoski <jovanvelickoski@gmail.com>
2025-08-27 13:15:11 -0400

    Added Second Charachter
    Added multiplayer scene and added second player sprite.

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -8,8 +8,13 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     
     private Rigidbody2D _rigidbody;
+<<<<<<< Updated upstream
     private Vector2 _moveInput; 
     public int _lastDirectionIndex = 0;
+=======
+    private Vector2 _moveInput;  
+    private int _lastDirectionIndex = 0;
+>>>>>>> Stashed changes
     private Animator _animator;
     
     private void Awake()
