813417f14e4e4a9b3b0a553870e31d7eee78cde2
Fish12355 <filip.lazarevski1212@gmail.com>
2025-08-27 19:11:03 +0200

    Added Player Attack
    Added player attack (shooting), bullet traveling with correct direction, enemy death after getting attacked.

---
+++ Assets/Scripts/Game/Bullet/Bullet.cs
@@ -0,0 +1,53 @@
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 6;
+    public Rigidbody2D rb;
+    private PlayerMovement playerMovement;
+    private int Timer = 0;
+
+    public int direction;
+
+    void Start()
+    {
+        if (direction == 0)
+        {
+            rb.linearVelocity = Vector2.down * speed;
+        }
+        else if (direction == 1)
+        {
+            rb.linearVelocity = Vector2.left * speed;
+        }
+        else if (direction == 2)
+        {
+            rb.linearVelocity = Vector2.up * speed;
+        }
+        else if (direction == 3)
+        {
+            rb.linearVelocity = Vector2.right * speed;
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.GetComponent<Skeleton>())
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Timer++;
+        if (Timer > 200)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
